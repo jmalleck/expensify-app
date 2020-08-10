9e6325c24648182ec82cf7071e23af32dc7f8c82
Joel Malleck <joelmalleck@gmail.com>
2020-08-10 12:17:47 -0400

    Initial commit

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,68 @@
+import expensesReducer from '../../reducers/expenses';
+
+import expenses from '../fixtures/expenses';
+
+test('should set default state', () => {
+    const state = expensesReducer(undefined, { type: '@@INIT' });
+
+    expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: expenses[1].id
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('should not remove expenses if id not found', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: '-1'
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
+});
+
+test('should add an expense', () => {
+
+    const newExpense = {
+        id: 2,
+        description: 'test',
+        note: 'hi',
+        amount: 10
+    };
+    const action = {
+        type: 'ADD_EXPENSE',
+        expense: newExpense
+    };
+
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([...expenses, newExpense]);
+});
+
+test('should edit an expense', () => {
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: expenses[1].id,
+        updates: {
+            note: 'test note'
+        }
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state[1].note).toBe('test note');
+});
+
+test('should not edit an expense if not found', () => {
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: 23456,
+        updates: {
+            note: 'test note'
+        }
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
+});
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// entry -> output
+const path = require('path');
+
+const publicPath = path.join(__dirname, 'public');
+
+// use webpack docs for more options
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: publicPath,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: publicPath,
+        historyApiFallback: true
+    }
+};
+
