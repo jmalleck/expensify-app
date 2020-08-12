ea489c2e1a23468de282139124fdddb91de7ad16
Joel Malleck <joelmalleck@gmail.com>
2020-08-11 20:43:37 -0400

    Add startEditExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses, startRemoveExpense } from '../../actions/expenses';
+import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses, startRemoveExpense, startEditExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -116,6 +116,7 @@ test('should fetch the expenses data from firebase', (done) => {
 test('should remove expenses from firebase', (done) => {
     const store = createMockStore();
     const id = expenses[2].id;
+
     store.dispatch(startRemoveExpense({ id })).then(() => {
         const actions = store.getActions();
         expect(actions[0]).toEqual({
@@ -125,6 +126,33 @@ test('should remove expenses from firebase', (done) => {
         return database.ref(`expenses/${id}`).once('value');
     }).then((snapshot) => {
         expect(snapshot.val()).toBeFalsy();
+        done();
+    });
+});
+
+test('should edit expenses from firebase', (done) => {
+    const store = createMockStore();
+    const id = expenses[0].id;
+    const updates = {
+        amount: 11111111,
+        note: 'test note'
+    };
+
+    store.dispatch(startEditExpense(id, updates)).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        });
+        return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual({
+            description: expenses[0].description,
+            amount: updates.amount,
+            note: updates.note,
+            createdAt: expenses[0].createdAt
+        });
         done();
     });
 });
